3149208c2a2efabaa854b851ba27279756acea18
chrismostert <15890652+chrismostert@users.noreply.github.com>
2021-12-01 13:23:11 +0100

    Less naive solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,23 +4,14 @@ fn main() {
         .map(|line| line.parse::<u32>().expect("Not a number!"))
         .collect();
 
-    let windows: Vec<u32> = input
-        .windows(3)
-        .map(|window| window.iter().sum::<u32>())
-        .collect();
-
-    fn get_n_inc(input: Vec<u32>) -> u32 {
-        let mut res = 0;
-
-        for i in 1..input.len() {
-            if input[i] > input[i - 1] {
-                res += 1;
-            }
-        }
-
-        res
+    fn get_increments(input: &[u32], window_size: usize) -> usize {
+        input
+            .iter()
+            .zip(input.iter().skip(window_size))
+            .filter(|(a, b)| b > a)
+            .count()
     }
-    
-    println!("Part 1: {}", get_n_inc(input));
-    println!("Part 2: {}", get_n_inc(windows));
+
+    println!("Part 1: {}", get_increments(&input, 1));
+    println!("Part 2: {}", get_increments(&input, 3));
 }
