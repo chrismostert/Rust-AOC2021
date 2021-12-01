487dcddbc0055d832977d974c0ab6a4d9cf6b130
chrismostert <15890652+chrismostert@users.noreply.github.com>
2021-12-01 12:01:39 +0100

    Working version day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,12 +4,23 @@ fn main() {
         .map(|line| line.parse::<u32>().expect("Not a number!"))
         .collect();
 
-    let mut n_inc = 0;
-    for i in 1..input.len() {
-        if input[i] > input[i-1] {
-            n_inc += 1;
-        }
-    }
+    let windows: Vec<u32> = input
+        .windows(3)
+        .map(|window| window.iter().sum::<u32>())
+        .collect();
 
-    println!("Part 1: {}", n_inc);
+    fn get_n_inc(input: Vec<u32>) -> u32 {
+        let mut res = 0;
+
+        for i in 1..input.len() {
+            if input[i] > input[i - 1] {
+                res += 1;
+            }
+        }
+
+        res
+    }
+    
+    println!("Part 1: {}", get_n_inc(input));
+    println!("Part 2: {}", get_n_inc(windows));
 }
