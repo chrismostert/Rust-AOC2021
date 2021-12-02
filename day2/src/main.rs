1eee4548c69e480f08a1d50d158183d8aaa987d6
chrismostert <15890652+chrismostert@users.noreply.github.com>
2021-12-02 12:50:33 +0100

    Shorter day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,40 +1,18 @@
-#[derive(Default)]
-struct Sub {
-    pos_horizontal: i32,
-    pos_depth: i32,
-    pos_depth_aim: i32,
-    aim: i32,
-}
-
-impl Sub {
-    fn execute_direction(&mut self, direction: &str) {
-        let splits = direction.split_once(" ").expect("Invalid instruction");
-        match (splits.0, splits.1.parse::<i32>().unwrap()) {
-            ("forward", val) => {
-                self.pos_horizontal += val;
-                self.pos_depth_aim += self.aim * val;
-            }
-            ("up", val) => {
-                self.pos_depth -= val;
-                self.aim -= val;
-            }
-            ("down", val) => {
-                self.pos_depth += val;
-                self.aim += val;
-            }
-            _ => unreachable!(),
-        }
-    }
-}
-
 fn main() {
     let input = include_str!("../input.txt");
-    let mut sub = Sub::default();
+    let (pos_x, depth, depth_aim, _) =
+        input
+            .lines()
+            .fold((0, 0, 0, 0), |(pos_x, depth, depth_aim, aim), direction| {
+                let splits = direction.split_once(" ").expect("Invalid instruction");
+                match (splits.0, splits.1.parse::<i32>().unwrap()) {
+                    ("forward", val) => (pos_x + val, depth, depth_aim + aim * val, aim),
+                    ("up", val) => (pos_x, depth - val, depth_aim, aim - val),
+                    ("down", val) => (pos_x, depth + val, depth_aim, aim + val),
+                    _ => unreachable!(),
+                }
+            });
 
-    for direction in input.lines() {
-        sub.execute_direction(direction);
-    }
-
-    println!("Part 1: {}", sub.pos_horizontal * sub.pos_depth);
-    println!("Part 2: {}", sub.pos_horizontal * sub.pos_depth_aim);
+    println!("Part 1: {}", pos_x * depth);
+    println!("Part 2: {}", pos_x * depth_aim);
 }
