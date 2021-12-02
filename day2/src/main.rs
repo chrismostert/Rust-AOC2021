9aa147ecfe002a3ca9ebdaa31553d3803a13b688
chrismostert <15890652+chrismostert@users.noreply.github.com>
2021-12-02 13:08:27 +0100

    Small improvement day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
     let input = include_str!("../input.txt");
-    let (pos_x, depth, depth_aim, _) =
+    let (pos_x, depth, aim) =
         input
             .lines()
-            .fold((0, 0, 0, 0), |(pos_x, depth, depth_aim, aim), direction| {
+            .fold((0, 0, 0), |(pos_x, depth, aim), direction| {
                 let splits = direction.split_once(" ").expect("Invalid instruction");
                 match (splits.0, splits.1.parse::<i32>().unwrap()) {
-                    ("forward", val) => (pos_x + val, depth, depth_aim + aim * val, aim),
-                    ("up", val) => (pos_x, depth - val, depth_aim, aim - val),
-                    ("down", val) => (pos_x, depth + val, depth_aim, aim + val),
+                    ("forward", val) => (pos_x + val, depth + aim * val, aim),
+                    ("up", val) => (pos_x, depth, aim - val),
+                    ("down", val) => (pos_x, depth, aim + val),
                     _ => unreachable!(),
                 }
             });
 
-    println!("Part 1: {}", pos_x * depth);
-    println!("Part 2: {}", pos_x * depth_aim);
+    println!("Part 1: {}", pos_x * aim);
+    println!("Part 2: {}", pos_x * depth);
 }
