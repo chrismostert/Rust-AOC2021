d12c91fd55de8d14ae9c96bb77fb609680479717
chrismostert <15890652+chrismostert@users.noreply.github.com>
2021-12-05 14:43:34 +0100

    Clean up range calculation by returning an iterator

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
+
 type Line = ((usize, usize), (usize, usize));
-const WIDTH: usize = 1000;
 
 fn parse_line(input: &str) -> Line {
     let (a, b) = input.split_once(" -> ").unwrap();
@@ -17,22 +18,21 @@ fn is_straight(line: &Line) -> bool {
     (a_x == b_x) | (a_y == b_y)
 }
 
+fn range(start: usize, end: usize) -> Box<dyn Iterator<Item = usize>> {
+    match start.cmp(&end) {
+        std::cmp::Ordering::Equal => Box::new(std::iter::repeat(start)),
+        std::cmp::Ordering::Greater => Box::new((end..start + 1).rev()),
+        std::cmp::Ordering::Less => Box::new(start..end + 1),
+    }
+}
+
 fn get_points(line: &Line, diagonal: bool) -> Vec<(usize, usize)> {
     let mut res = vec![];
     let ((a_x, a_y), (b_x, b_y)) = *line;
 
-    if is_straight(line) {
-        for i in if a_x <= b_x {a_x..b_x+1} else {b_x..a_x+1} {
-            for j in if a_y <= b_y {a_y..b_y+1} else {b_y..a_y+1} {
-                res.push((i,j));
-            }
-        }
-    } else if diagonal {
-        let xrange: Vec<usize> = if a_x <= b_x {(a_x..b_x+1).collect()} else {(b_x..a_x+1).rev().collect()};
-        let yrange: Vec<usize> = if a_y <= b_y {(a_y..b_y+1).collect()} else {(b_y..a_y+1).rev().collect()};
-
-        for (&x,&y) in xrange.iter().zip(yrange.iter()) {
-            res.push((x,y));
+    for (x, y) in range(a_x, b_x).zip(range(a_y, b_y)) {
+        if is_straight(line) | diagonal {
+            res.push((x, y));
         }
     }
 
@@ -40,18 +40,15 @@ fn get_points(line: &Line, diagonal: bool) -> Vec<(usize, usize)> {
 }
 
 fn get_answer(input: &[Line], diagonal: bool) -> usize {
-    let points = input
-            .iter()
-            .map(|line| get_points(line, diagonal));
-    
-    let mut grid = vec![vec![0; WIDTH]; WIDTH];
-    
-    for point in points.flatten() {
-        grid[point.0][point.1] += 1;
-    }
-    grid.iter().flatten().filter(|&num| *num >= 2).count()
-}
+    let points = input.iter().map(|line| get_points(line, diagonal));
+    let mut grid: HashMap<(usize, usize), i32> = HashMap::new();
 
+    for point in points.flatten() {
+        *grid.entry((point.0, point.1)).or_insert(0) += 1;
+    }
+    
+    grid.iter().filter(|(_, &v)| v >= 2).count()
+}
 
 fn main() {
     let input: Vec<Line> = include_str!("../input.txt")
@@ -60,5 +57,5 @@ fn main() {
         .collect();
 
     println!("Part 1: {}", get_answer(&input, false));
-    println!("Part 2: {}", get_answer(&input, true));     
+    println!("Part 2: {}", get_answer(&input, true));
 }
