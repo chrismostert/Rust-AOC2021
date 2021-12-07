bf8fc4512ea693e39676de4dab156e7d2d981d25
chrismostert <15890652+chrismostert@users.noreply.github.com>
2021-12-07 17:30:40 +0100

    Day 7. Part 2 is roughly the mean because n^2+n ~ n^2 (give or take 0.5)

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -7,15 +7,33 @@ fn parse_input(input: &str) -> Vec<i32> {
 }
 
 fn p1(input: &[i32]) -> i32 {
-    let mut sorted = input.to_owned();
-    let l = input.len() / 2;
-    sorted.sort_unstable();
+    input
+        .iter()
+        .map(|&num| (num - statistical::median(input)).abs())
+        .sum()
+}
 
-    let pos = ((sorted[l] + sorted[l - 1]) / 2i32).abs();
-    input.iter().map(|num| (num - pos).abs()).sum()
+fn triangle(dist: i32) -> i32 {
+    (dist * (dist + 1)) / 2
+}
+
+fn p2(input: &[f32]) -> i32 {
+    let mean = statistical::mean(input);
+    Ord::min(
+        input
+            .iter()
+            .map(|&num| triangle((num as i32 - mean.floor() as i32).abs()))
+            .sum(),
+        input
+            .iter()
+            .map(|&num| triangle((num as i32 - mean.ceil() as i32).abs()))
+            .sum(),
+    )
 }
 
 fn main() {
     let input = parse_input(include_str!("../input.txt"));
+    let input_float = input.iter().map(|&x| x as f32).collect::<Vec<f32>>();
     println!("Part 1: {}", p1(&input));
+    println!("Part 2: {}", p2(&input_float));
 }
