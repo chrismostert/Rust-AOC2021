71aa67c24629ed129358ce026d3de3860c96359f
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2021-12-13 13:47:59 +0100

    Cleanup

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -1,22 +1,27 @@
-use std::collections::HashMap;
-type Grid = HashMap<(usize, usize), bool>;
+use std::collections::HashSet;
+type Grid = HashSet<(usize, usize)>;
 
 enum FoldInstruction {
     Up(usize),
-    Left(usize)
+    Left(usize),
 }
 
 fn make_grid(input: &str) -> Grid {
-    input.lines().fold(HashMap::default(), |mut acc, x| {
-        let (x,y) = x.split_once(',').unwrap();
-        acc.insert((x.parse().unwrap(), y.parse().unwrap()), true);
+    input.lines().fold(HashSet::default(), |mut acc, x| {
+        let (x, y) = x.split_once(',').unwrap();
+        acc.insert((x.parse().unwrap(), y.parse().unwrap()));
         acc
     })
 }
 
-fn get_instructions(input: &'static str) -> impl Iterator<Item=FoldInstruction> {
+fn get_instructions(input: &'static str) -> impl Iterator<Item = FoldInstruction> {
     input.lines().map(|line| {
-        let (t, v) = line.split_whitespace().nth(2).unwrap().split_once('=').unwrap();
+        let (t, v) = line
+            .split_whitespace()
+            .nth(2)
+            .unwrap()
+            .split_once('=')
+            .unwrap();
         match t {
             "x" => FoldInstruction::Left(v.parse().unwrap()),
             "y" => FoldInstruction::Up(v.parse().unwrap()),
@@ -26,9 +31,13 @@ fn get_instructions(input: &'static str) -> impl Iterator<Item=FoldInstruction> 
 }
 
 fn grid_size(grid: &Grid) -> (usize, usize) {
-    grid.keys().fold((0,0), |mut acc, x| {
-        if x.0+1 > acc.0 {acc.0 = x.0+1;};
-        if x.1+1 > acc.1 {acc.1 = x.1+1;};
+    grid.iter().fold((0, 0), |mut acc, x| {
+        if x.0 + 1 > acc.0 {
+            acc.0 = x.0 + 1;
+        };
+        if x.1 + 1 > acc.1 {
+            acc.1 = x.1 + 1;
+        };
         acc
     })
 }
@@ -37,7 +46,7 @@ fn print_grid(grid: &Grid) {
     let (w, h) = grid_size(grid);
     for y in 0..h {
         for x in 0..w {
-            match grid.get(&(x,y)) {
+            match grid.get(&(x, y)) {
                 Some(_) => print!("#"),
                 None => print!(" "),
             }
@@ -51,20 +60,20 @@ fn do_fold(grid: &mut Grid, instruction: FoldInstruction) {
     match instruction {
         FoldInstruction::Up(line) => {
             for x in 0..w {
-                for y in line+1..h {
-                    if grid.get(&(x,y)).is_some() {
-                        grid.insert((x, line-(y-line)), true);
-                        grid.remove(&(x,y));
+                for y in line + 1..h {
+                    if grid.get(&(x, y)).is_some() {
+                        grid.insert((x, line - (y - line)));
+                        grid.remove(&(x, y));
                     }
                 }
             }
-        },
+        }
         FoldInstruction::Left(line) => {
-            for x in line+1..w {
+            for x in line + 1..w {
                 for y in 0..h {
-                    if grid.get(&(x,y)).is_some() {
-                        grid.insert((line-(x-line), y), true);
-                        grid.remove(&(x,y));
+                    if grid.get(&(x, y)).is_some() {
+                        grid.insert((line - (x - line), y));
+                        grid.remove(&(x, y));
                     }
                 }
             }
@@ -80,8 +89,8 @@ fn main() {
     for (i, instruction) in instructions.enumerate() {
         do_fold(&mut grid, instruction);
         if i == 0 {
-            println!("Part 1: {}", grid.keys().count());
-        }  
+            println!("Part 1: {}", grid.len());
+        }
     }
     println!("Part 2:");
     print_grid(&grid);
