dc9f30033ccbf9d5e917e16189b7e991f75bda14
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2021-12-14 20:01:55 +0100

    Itertools magic

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,75 +1,51 @@
+use itertools::Itertools;
 use std::collections::HashMap;
-use std::str;
 
-fn get_slices(input: &str, size: usize) -> impl Iterator<Item = &str> {
+fn get_rules(input: &str) -> HashMap<(char, char), char> {
     input
-        .as_bytes()
-        .windows(size)
-        .map(|x| str::from_utf8(x).unwrap())
-}
-
-fn get_counts<'a>(slices: impl Iterator<Item = &'a str>) -> HashMap<String, usize> {
-    slices.fold(HashMap::default(), |mut acc, x| {
-        *acc.entry(x.to_owned()).or_insert(0) += 1;
-        acc
-    })
-}
-
-fn get_rules(input: &str) -> HashMap<&str, &str> {
-    input.lines().map(|x| x.split_once(" -> ").unwrap()).fold(
-        HashMap::default(),
-        |mut acc, (pair, insertion)| {
-            acc.insert(pair, insertion);
-            acc
-        },
-    )
+        .lines()
+        .filter_map(|x| x.split_once(" -> "))
+        .map(|(pair, insertion)| {
+            (
+                (pair.chars().next().unwrap(), pair.chars().nth(1).unwrap()),
+                insertion.chars().next().unwrap(),
+            )
+        })
+        .collect::<HashMap<_, _>>()
 }
 
 fn do_step(
-    pair_counts: &mut HashMap<String, usize>,
-    char_counts: &mut HashMap<String, usize>,
-    rules: &HashMap<&str, &str>,
+    pair_counts: &mut HashMap<(char, char), usize>,
+    char_counts: &mut HashMap<char, usize>,
+    rules: &HashMap<(char, char), char>,
 ) {
-    let cur_pairs = pair_counts.clone();
-    for (pair, amount) in cur_pairs {
-        if let Some(&to_insert) = rules.get(&*pair) {
-            match pair_counts[&pair].saturating_sub(amount) {
-                0 => {
-                    pair_counts.remove(&pair);
-                }
-                amount => {
-                    *pair_counts.get_mut(&pair).unwrap() = amount;
-                }
-            }
-
-            for to_inc in [
-                format!("{}{}", &pair[..1], to_insert),
-                format!("{}{}", to_insert, &pair[1..]),
-            ] {
-                *pair_counts.entry(to_inc).or_insert(0) += amount;
-            }
-
-            *char_counts.entry(to_insert.to_owned()).or_insert(0) += amount;
+    let old_counts = pair_counts.clone();
+    for ((a, b), n) in old_counts {
+        if let Some(&insertion) = rules.get(&(a, b)) {
+            *pair_counts.entry((a, b)).or_insert(0) -= n;
+            *pair_counts.entry((a, insertion)).or_insert(0) += n;
+            *pair_counts.entry((insertion, b)).or_insert(0) += n;
+            *char_counts.entry(insertion).or_insert(0) += n;
         }
     }
-}
-
-fn get_ans(char_counts: &HashMap<String, usize>) -> usize {
-    char_counts.values().max().unwrap() - char_counts.values().min().unwrap()
 }
 
 fn main() {
     let (polymer_input, rules_input) = include_str!("../input.txt").split_once("\n\n").unwrap();
-
-    let mut pair_counts = get_counts(get_slices(polymer_input, 2));
-    let mut char_counts = get_counts(get_slices(polymer_input, 1));
+    let mut pair_counts = polymer_input
+        .chars()
+        .tuple_windows()
+        .map(|(a, b)| (a, b))
+        .counts();
+    let mut char_counts = polymer_input.chars().counts();
     let rules = get_rules(rules_input);
+    let ans = |c: &HashMap<_, _>| c.values().max().unwrap() - c.values().min().unwrap();
 
     for i in 0..40 {
         do_step(&mut pair_counts, &mut char_counts, &rules);
         if i == 9 {
-            println!("Part 1: {}", get_ans(&char_counts));
+            println!("Part 1: {}", ans(&char_counts));
         }
     }
-    println!("Part 2: {}", get_ans(&char_counts));
+    println!("Part 2: {}", ans(&char_counts));
 }
